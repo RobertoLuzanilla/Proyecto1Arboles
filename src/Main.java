9d2ddfdfc011fce6d81fc21622ea1e5625daedff
Roberto017u7 <luzanillasanchez@gmail.com>
2023-05-08 23:47:02 -0700

    Ahora si ya esta terminado y probado

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,31 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        String text, letras = "abcdefghijklmnñopqrstuvwxyz ";
+
         Oper_Arboles OA = new Oper_Arboles();
 
-        System.out.println("Ingrese un texto: ");
-        text=sc.nextLine().toLowerCase();
+        System.out.println("Ingrese texto");
+        String text = sc.nextLine().toLowerCase();
+
+        String signos = "abcdefghijklmnñopqrstuvwxyz ";
 
         for (int i = 0; i < text.length(); i++) {
-            if (!letras.contains(String.valueOf(text.charAt(i)))){
-                text = text.substring(0,i) + text.substring(i + 1);
-            }
+            if (!signos.contains(String.valueOf(text.charAt(i))))
+                text = text.substring(0, i) + text.substring(i + 1);
         }
-        String textS[]=text.split(" ", 0);
-        for (String palabras : textS);
 
+        String TextS[] = text.split(" ", 0);
+
+        for (String palabra : TextS)
+            OA.insertar(OA.raiz, palabra);
+
+        System.out.println();
         OA.preorden(OA.raiz);
 
     }
+
 }
